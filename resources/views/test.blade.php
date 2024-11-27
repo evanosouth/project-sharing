1b11564194f4f206a7bafb997b3de492f786d0a3
mping593 <mping593@gmail.com>
2024-11-27 13:46:20 +0000

    commit mping 3

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -9,5 +9,24 @@
     <h3>Hello World!!!</h3>
 
     <h2>Hello World dari Mping</h2>
+
+    fkjbfksdbf
+
+    sdvsd
+    vsd
+    variant_date_to_timestampvsd
+    <var>variant_date_to_timestampvsdvsd
+        variant_date_to_timestampvsdvsdvsdv
+        sdv
+        sd
+        <var>sv
+            sv
+            ds
+            <var>vs
+                vs
+                v
+            </var>
+        </var>
+    </var>
 </body>
 </html>
