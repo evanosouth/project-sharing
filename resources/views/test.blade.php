dd498b5adaef056b25537d39963752c892cb6064
mping593 <mping593@gmail.com>
2024-11-27 14:02:19 +0000

    commit mping lagi'

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -9,5 +9,31 @@
     <h3>Hello World!!!</h3>
 
     <h2>Hello World dari Mping</h2>
+    <h2>Hello World dari Mping Terbaru</h2>
+    sdv
+    sv
+    frg
+    hash_hmac_file
+    dghd
+    <h1>df
+        dfb
+        df
+        <b><b>
+
+        </b></b>
+    </h1>
+    abstract
+    av
+    dv
+    adva
+    dva
+    <var>adv
+        <data value="vadv
+        a
+        
+        "></data>
+    </var>
+    ca
+    session_cache_expire
 </body>
 </html>
